25af0647b09ce33083ec6d6efdc08bc91a8c51b7
Abhinav Anand <abhinavanand072@gmail.com>
2026-05-31 07:34:20 +0530

    sending live price from server

--- controllers/livePriceController.js
+++ controllers/livePriceController.js
@@ -1,5 +1,18 @@
-export function livePriceController(req, res) {
-    res.json({message: 'Getting Live Price'})
+import { prices } from "../data/prices.js"
 
-    console.log('Sending live price')
+export function livePriceController(req, res) {
+    res.setHeader('Content-Type', 'text/event-stream')
+    res.setHeader('Cache-Controll', 'no-cache')
+    res.setHeader('Connection', 'keep-alive')
+
+    setInterval(() => {
+        const randomIndex = Math.floor(Math.random() * prices.length)
+
+        res.write(
+            `data: ${JSON.stringify({
+                event: 'price-update',
+                price: prices[randomIndex]
+            })} \n\n`
+        )
+    }, 3000)
 }
