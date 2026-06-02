c44704bb445d3f7ffbd8c611533d1f0565144389
Abhinav Anand <abhinavanand072@gmail.com>
2026-06-02 08:41:09 +0530

    successfully send purchase data and showing dialog

--- public/index.js
+++ public/index.js
@@ -1,16 +1,43 @@
 const investmentForm = document.getElementById('investment-form')
 const investmentAmount = document.getElementById('investment-amount')
+const priceDisplay = document.getElementById('price-display')
+const outputsDialog = document.getElementById('outputs')
+const investmentSummary = document.getElementById('investment-summary')
+const dialogCloseBtn = document.getElementById('dialog-close-btn')
 
-investmentForm.addEventListener('submit', function(e) {
+investmentForm.addEventListener('submit', async function(e) {
     e.preventDefault()
+
+    const amountPaid = Number(investmentAmount.value)
+    const pricePerOz = Number(priceDisplay.textContent)
+    const goldSold = Number(amountPaid/pricePerOz).toFixed(4)
     
     const data = {
-        "date": new Date().toISOString(),
-        "amount-paid": `￡${investmentAmount.value}`,
-        "price per Oz": "£2009.57",
-        "gold sold": `${(Number(investmentAmount.value)/2009.57).toFixed(4)} OZ`
+        date: new Date().toISOString(),
+        amount_paid: `￡${amountPaid}`,
+        price_per_Oz: `￡${pricePerOz}`,
+        gold_sold: `${goldSold} Oz`
     }
 
-    console.log(data)
+    const res = await fetch('http://localhost:8000/api', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    })
 
+    const resData = await res.json()
+
+    console.log(resData)
+
+    outputsDialog.showModal();
+    investmentSummary.textContent = `
+        You just bought ${resData.gold_sold} of gold for ${resData.amount_paid}. \n You will receive documentation shortly.
+    `
+
+})
+
+dialogCloseBtn.addEventListener('click', function(e) {
+    outputsDialog.close()
 })
