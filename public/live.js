8678506b5a92edfb1b00925118177c5b93ff9dcb
Abhinav Anand <abhinavanand072@gmail.com>
2026-06-02 07:49:18 +0530

    successfully displaying live prices

--- public/live.js
+++ public/live.js
@@ -1,11 +1,17 @@
 const eventSource = new EventSource('http://localhost:8000/api/liveprice')
 
 const priceDisplay = document.getElementById('price-display')
+const connectionStatus = document.getElementById('connection-status')
 
 eventSource.onmessage = (event) => {
-    console.log(event.data)
+    const data = JSON.parse(event.data)
+    const price = data.price
+
+    connectionStatus.textContent = `Live Price 🟢`
+    priceDisplay.textContent = price
 }
 
 eventSource.onerror = (err) => {
-    console.log(err.data)
+    connectionStatus.textContent = `Offline 🔴`
+    priceDisplay.textContent = `----.--`
 }
